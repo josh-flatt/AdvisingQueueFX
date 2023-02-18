dea23011f3422f3605fa0d6df824f3204c90204b
Josh Flatt <josh.m.flatt@gmail.com>
2023-02-18 11:57:10 -0700

    finished old implementation, works properly

--- AdvisingQueue/src/main/java/com/example/advisingqueue/Application.java
+++ AdvisingQueue/src/main/java/com/example/advisingqueue/Application.java
@@ -3,6 +3,7 @@ package com.example.advisingqueue;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.layout.BorderPane;
 
 import java.io.IOException;
 
@@ -10,7 +11,7 @@ public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
