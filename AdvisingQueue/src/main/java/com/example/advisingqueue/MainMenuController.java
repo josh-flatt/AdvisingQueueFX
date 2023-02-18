a77b76dd11694bd293ed1467e3bad8a1d8d1a6fa
Josh Flatt <josh.m.flatt@gmail.com>
2023-02-18 11:36:49 -0700

    renamed application files

--- AdvisingQueue/src/main/java/com/example/advisingqueue/MainMenuController.java
+++ AdvisingQueue/src/main/java/com/example/advisingqueue/MainMenuController.java
@@ -3,7 +3,7 @@ package com.example.advisingqueue;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class MainMenuController {
     @FXML
     private Label welcomeText;
 
